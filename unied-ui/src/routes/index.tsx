a6be668c611ceb3be1e47a8bb56b73b47482502a
dmachava <dmachava@luwali.co.mz>
2025-12-18 10:08:07 +0200

    Feat: Building project

---
+++ unied-ui/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                {/* Rotas Públicas */}
+                {/* <Route path="/" element={<Home />} /> */}
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRoutes;
